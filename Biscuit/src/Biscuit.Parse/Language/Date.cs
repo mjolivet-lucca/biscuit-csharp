1fd020b485ea77428f7b68657fc081016db4884f
Mathieu jolivet <mjolivet@lucca.fr>
2022-10-19 19:33:16 +0200

    Add date

---
+++ Biscuit/src/Biscuit.Parse/Language/Date.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Biscuit.Parse.Language;
+
+public class Date : IPrimordialData
+{
+    public string InputString { get; }
+    public DateTime Value { get; set; }
+
+    public Date(string inputString)
+    {
+        InputString = inputString;
+        Value = IsValid() && ParseDate(out var result) ? result : DateTime.MinValue;
+    }
+    public bool IsValid()
+    {
+        return !string.IsNullOrWhiteSpace(InputString) && ParseDate(out _);
+    }
+
+    private bool ParseDate(out DateTime result)
+    {
+        return ParseWithFormat(out result, "yyyy-MM-ddTHH:mm:ss")
+            || ParseWithFormat(out result, "yyy-MM-ddTHH:mm:ss")
+            || ParseWithFormat(out result, "yy-MM-ddTHH:mm:ss")
+            || ParseWithFormat(out result, "y-MM-ddTHH:mm:ss")
+            || ParseWithFormat(out result, "yyyy-MM-ddTHH:mm:ssZ")
+            || ParseWithFormat(out result, "yyyy-MM-ddTHH:mm:sszzz");
+    }
+
+    private bool ParseWithFormat(out DateTime result, string format)
+    {
+        return DateTime.TryParseExact(InputString, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
+    }
+}
---
+++ Biscuit/test/Biscuit.Parse.Test/Language/DateTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+using Biscuit.Parse.Language;
+using Xunit;
+
+namespace Biscuit.Parse.Test.Language;
+
+public class DateTest
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData(" ")]
+    public void DateShouldBeInvalidWhenNullOrEmpty(string inputData)
+    {
+        var date = new Date(inputData);
+
+        var isValid = date.IsValid();
+
+        Assert.False(isValid);
+        Assert.Equal(DateTime.MinValue, date.Value);
+    }
+
+    [Fact]
+    public void NonDateFormatShouldBeInvalid()
+    {
+        var date = new Date("aze23");
+
+        var isValid = date.IsValid();
+
+        Assert.False(isValid);
+        Assert.Equal(DateTime.MinValue, date.Value);
+    }
+
+    [Theory]
+    [InlineData("20221203")]
+    [InlineData("2022/12/03")]
+    [InlineData("17/05/1982")]
+    public void NonIso8601DateShouldNotBeValid(string exampleDate)
+    {
+        var date = new Date(exampleDate);
+
+        var isValid = date.IsValid();
+
+        Assert.False(isValid);
+        Assert.Equal(DateTime.MinValue, date.Value);
+    }
+
+    [Theory]
+    [InlineData("2022-12-03T11:37:28")]
+    [InlineData("522-12-03T11:37:28")]
+    [InlineData("22-12-03T11:37:28")]
+    [InlineData("2-12-03T11:37:28")]
+    [InlineData("2022-12-03T11:37:28Z")]
+    [InlineData("2022-12-03T11:37:28+05:00")]
+    [InlineData("2022-12-03T11:37:28-05:00")]
+    public void Iso8601DateShouldBeValidWith3Formats(string exampleDate)
+    {
+        var date = new Date(exampleDate);
+
+        var isValid = date.IsValid();
+        
+        Assert.True(isValid);
+        Assert.NotEqual(DateTime.MinValue, date.Value);
+    }
+}
